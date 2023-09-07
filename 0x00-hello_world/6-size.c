e8bc3b0349e49c8dc0c4adbd6bceb13d5670cb27
root <root@Nickszn>
2023-09-07 10:43:23 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	printf("Size of a char\n:"
+			sizeof(a);
+	printf("Size of a char\n:"
+                        sizeof(b);
+	printf("Size of a char\n:"
+                        sizeof(c);
+	printf("Size of a char\n:"
+                        sizeof(d);
+        printf("Size of a char\n:"
+                        sizeof(e);
+     return (0);
+}
