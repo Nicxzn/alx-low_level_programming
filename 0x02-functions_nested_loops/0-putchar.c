6a70d6cff1fa5e38ac39272293eeb7dae6504d44
root <root@Nickszn>
2023-09-12 13:14:46 -0700

    _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,9 @@
 /**
  * main - Entry point
  *
- * Return : 0
- *
-*/
-main(void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -16,5 +15,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
